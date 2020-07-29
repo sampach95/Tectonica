3f46776e0ed67238d3e8681de12ba8960646a72c
sampach95 <60448621+sampach95@users.noreply.github.com>
2020-07-28 21:02:20 -0500

    Add files via upload

---
+++ paradas.js
@@ -0,0 +1,14 @@
+var paradas = {
+"type": "FeatureCollection",
+"name": "paradas_gcs",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "LABEL": "A", "DIA": 1, "NOMBRE": "El Jabalí", "DESCRIPCIÓN": "afloramientos en arroyo", "X": 418307, "Y": 2306980 }, "geometry": { "type": "Point", "coordinates": [ -99.785305184097595, 20.861147952428201, 0.0 ] } },
+{ "type": "Feature", "properties": { "LABEL": "B", "DIA": 1, "NOMBRE": "San Juan de la Rosa", "DESCRIPCIÓN": "Arroyo", "X": 419782, "Y": 2297486 }, "geometry": { "type": "Point", "coordinates": [ -99.770690863560247, 20.775433947943988, 0.0 ] } },
+{ "type": "Feature", "properties": { "LABEL": "C", "DIA": 2, "NOMBRE": "Formación Tamaulipas", "DESCRIPCIÓN": "pliegues", "X": 446944, "Y": 2310934 }, "geometry": { "type": "Point", "coordinates": [ -99.510152151401556, 20.897915591003787, 0.0 ] } },
+{ "type": "Feature", "properties": { "LABEL": "D", "DIA": 2, "NOMBRE": "Formación Trancas", "DESCRIPCIÓN": "Pliegue “El Piñón”", "X": 449755, "Y": 2311015 }, "geometry": { "type": "Point", "coordinates": [ -99.483126483346382, 20.898725993030688, 0.0 ] } },
+{ "type": "Feature", "properties": { "LABEL": "E", "DIA": 2, "NOMBRE": "Formación El Doctor", "DESCRIPCIÓN": null, "X": 438776, "Y": 2306320 }, "geometry": { "type": "Point", "coordinates": [ -99.588525037240601, 20.85597427645861, 0.0 ] } },
+{ "type": "Feature", "properties": { "LABEL": "F", "DIA": 3, "NOMBRE": "San José de la Cal.", "DESCRIPCIÓN": "Peña de Bernal costado norte", "X": 401233, "Y": 2296523 }, "geometry": { "type": "Point", "coordinates": [ -99.948828527635598, 20.765841504867474, 0.0 ] } },
+{ "type": "Feature", "properties": { "LABEL": "G", "DIA": 3, "NOMBRE": "Peña de Bernal.", "DESCRIPCIÓN": null, "X": 401449, "Y": 2294354 }, "geometry": { "type": "Point", "coordinates": [ -99.946631683353772, 20.746256759977186, 0.0 ] } }
+]
+};
